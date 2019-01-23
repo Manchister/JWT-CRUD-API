788ff51b98ea7c456654712a1df1ff3874cda3dd
Manchister <manchister@ymail.com>
2019-01-23 12:37:25 +0200

    finish rigister and login controllers

---
+++ app/Http/Controllers/APILoginController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers;
+use App\User;
+use JWTFactory;
+use JWTAuth;
+use Response;
+use Validator;
+
+class APILoginController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|max:255',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $cred = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($cred)) {
+                return response()->json(['error', 'invalid email or password', [401]]);
+            }
+        } catch (JWTExp $e) {
+            return response()->json(['error', 'could not create the token', [500]]);
+        }
+
+        return response()->json(compact('token'));
+    }
+
+
+    // public function login(Request $request)
+    // {
+    //     $validator = Validator::make($request->all(),[
+    //         'email' => 'required|string|max:255',
+    //         'password' => 'required',
+    //     ]);
+
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->errors());
+    //     }
+
+    //     $cred = $request->only('email', 'password');
+
+    //     try {
+    //         if (! $token = JWTAuth::attempt($cred)) {
+    //             return response()->json(['error' => 'invalid email or password',[401]]);
+    //         }
+    //     } catch (JWTAuthException $e ) {
+    //         return response()->json(['error' => 'could not creat the token',[500]]);
+    //     }
+
+    //     return response()->json(compact('token'));
+    // }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('user')->group(function() {
+    Route::post('register', 'APIRegisterController@register');
+    Route::post('login', 'APILoginController@login');
+});
